350c2c332a1b20f84dd4d4e74b6c215a94662e65
github username <github email address>
2018-06-09 12:46:51 -0400

    first draft

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+require "pry"
 class Triangle
   # write code here
+attr_accessor :a, :b, :c, :type
+  def initialize(a, b, c)
+    sides_array =[a, b, c]
+    sides_array.sort!
+    if (sides_array[0] + sides_array[1]) > sides_array[2] && sides_array[0] > 0
+      @a, @b, @c = a, b, c
+    else
+        raise TriangleError
+    end
+  end
+
+  def kind
+      if a == b && b == c && c == a
+        self.type = :equilateral
+      elsif a == b || b == c || a == c
+        self.type = :isosceles
+      elsif a != b && b != c && a != c
+        self.type = :scalene
+      else
+        self.type = triangle
+      end
+  end
+
+      class TriangleError < StandardError
+        def message
+          "The sides of your triangle are not positive and does not pass the traingle inequality test "
+        end
+      end
+
+
 end
